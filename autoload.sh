68a8fac63c7b49db93024d530d46f9d1b2cf835b
Aaron Senecal <aaron.senecal@acquia.com>
2021-09-06 16:26:23 -0700

    Autoloader script needs base directory context.

--- autoload.sh
+++ autoload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # First, automatically discover and load exports and functions for core utilities.
-for script in $(find ./core -type f -perm -u+r 2>/dev/null | xargs); do
+for script in $(find $(dirname ${BASH_SOURCE[0]})/core -type f -perm -u+r 2>/dev/null | xargs); do
   source $script; 
 done
 
 # Next, automatically discover and load user-created exports and functions.
-for script in $(find ./autoload -type f -perm -u+r 2>/dev/null | xargs); do
+for script in $(find $(dirname ${BASH_SOURCE[0]})/autoload -type f -perm -u+r 2>/dev/null | xargs); do
   source $script; 
 done
